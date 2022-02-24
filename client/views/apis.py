63434362d4ac97968b70157c257972e802a84d57
Mohammad Rahimi <moyi.pary@gmail.com>
2022-02-25 03:25:36 +0330

    1- complete ajax in uni_request_submit.html 2- edit save method in client.models.py 3- add .gitignore 4- ignore settings.py (for database)

--- client/views/apis.py
+++ client/views/apis.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
 from .imports import *
 from client.models import *
+from django.db.models import Q
+
 
 @login_required
 def get_document(request):
     context = []
-    for i in Owner_document.objects.filter(owner=request.user.id):
-        context.append({'id': i.pk, 'title': i.title})
+    q = Q(owner=request.user)
+    for i in Owner_document.objects.filter(q & ~Q(image=None)):
+        if i.image:
+            context.append({'id': i.pk, 'title': i.title, 'image': i.image.url})
     return JsonResponse(context, safe=False)
--- client/views/user_document.py
+++ client/views/user_document.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+
 from .imports import *
 from ..models import Owner_document
 
@@ -7,6 +9,7 @@ def user_document(request):
     context['documents'] = Owner_document.objects.filter(owner=request.user.id)
     return render(request, 'client/user_document.html', context)
 
+
 def user_document_upload(request):
     context = {}
     if request.method == "POST":
@@ -14,8 +17,10 @@ def user_document_upload(request):
         document = Owner_document()
         if 'owner_upload' in request.FILES:
             document.image = request.FILES['owner_upload']
-        document.owner_id = request.user.id
+        document.owner = request.user
         document.title = context['title']
         document.save()
         context['result'] = True
+        context['document'] = document
+        return HttpResponseRedirect(reverse('educational:uni_request_submit'))
     return render(request, 'client/user_document_upload.html', context)
