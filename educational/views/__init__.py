db82fa0049d1c923deb6e152b0796ff1d06ae5ab
Mohammad Rahimi <moyi.pary@gmail.com>
2022-03-31 11:46:06 +0430

    Just formatting the code.

--- educational/views/__init__.py
+++ educational/views/__init__.py
@@ -2,7 +2,9 @@ from .universities import *
 from .year_semester import *
 from .apis import *
 from .degree_field_study import *
-from .semesters import *
+from .semesters import semesters
+from .new_semester import new_semester
+from .submit_semester import submit_semester
 from .uni_request import *
 from .term_status import term_status
 from .reports import reports
